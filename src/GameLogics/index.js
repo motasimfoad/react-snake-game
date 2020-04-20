1cc757d673f6f0751cdfaeaad01c1a8b954bf051
Motasim Foad <motasimfoad@gmail.com>
2020-04-21 01:43:11 +0600

    Trying to move snake

--- src/GameLogics/index.js
+++ src/GameLogics/index.js
@@ -5,3 +5,32 @@ export const foodPosition = () => {
     let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
     return [x,y]
 }
+
+export const moveSnake = (props) => {
+    let position = [props.snakeBody];
+    let head = position[position.length-1];
+    let copyDirection = props.direction;
+
+    switch (copyDirection){
+        case 'RIGHT':
+        head = [head[0] + 2, head[1]];
+        break;
+        case 'LEFT':
+        head = [head[0] - 2, head[1]];
+        break;
+         case 'DOWN':
+        head = [head[0], head[1] + 2];
+        break;
+        case 'UP':
+        head = [head[0], head[1] - 2];
+        break;
+    }
+    position.push(head);
+    position.shift();
+    let np = position
+    
+
+
+    
+return np;
+}
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,13 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Home/home.css';
 import Snake from '../../Components/snake';
 import Food from '../../Components/snake_food';
-import {foodPosition} from '../../GameLogics';
+import {foodPosition, moveSnake} from '../../GameLogics';
 
 function Home() {
 
 const [snakeBody, setSnakeBody] = useState([[0,0],[2,0]]);
 const [snakeFood, setSnakeFood] = useState(foodPosition());
+const [direction, setDirection] = useState('RIGHT');
+const [gameSpeed, setGameSpeed] = useState(200);
+
+useEffect(() => {  
+      document.onkeydown = onkeydown;
+    //   setInterval(() => {
+    //     setSnakeBody(moveSnake({snakeBody, direction}));
+    //   }, 500);
+      
+      console.log(direction);
+      let a = moveSnake({snakeBody, direction});
+      console.log(a);
+      
+    });
+
+onkeydown = (e) =>{
+   e = e || window.event;
+   switch (e.keyCode){
+       case 37:
+        setDirection('LEFT');
+           break;
+       case 38:
+        setDirection('UP');
+           break;
+       case 39:
+        setDirection('RIGHT');
+           break;
+       case 40:
+        setDirection('DOWN');
+           break;
+   }
+};
 
   return (
     <div className="app">
