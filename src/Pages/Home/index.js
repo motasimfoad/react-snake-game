65d5c03748cef6132969c223b31abc5027523358
Motasim Foad <motasimfoad@gmail.com>
2020-04-20 17:13:43 +0600

    The snake has landedgit add .

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Home/home.css';
+import Snake from '../../Components/snake'
 
 function Home() {
+
+const [snakeBody, setSnakeBody] = useState([[0,0],[2,0]]);
+
   return (
-    <div className="App">
-      Home
+    <div className="app">
+      <div className="game-board">
+        <Snake snakeBody={snakeBody} />
+      </div>
     </div>
   );
 }
