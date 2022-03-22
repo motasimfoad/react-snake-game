da066dd30729093d58243391a65906e35ab21b82
HrikB <hrik.bhowal@gmail.com>
2022-03-21 21:31:12 -0700

    config

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      {connected ? <Home /> : <Connect />}
+      {connected ? <Home /> : <Connect setConnected={setConnected} />}
     </>
   );
 }
--- src/Pages/Connect/index.js
+++ src/Pages/Connect/index.js
@@ -1,10 +1,29 @@
 
 import React from 'react'
+import "./connect.css"
+import Web3Modal from "web3modal"
 
-function Connect() {
+const connectionConfig = {
+    cacheProvider: true,
+    network: "rinkeby",
+}
+
+function Connect({ setConnected }) {
+
+    const connect = async () => {
+        const web3Modal = new Web3Modal(connectionConfig)
+
+        try {
+            const provider = await web3Modal.connect();
+            setConnected(true)
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div className="connect__container">
-            <button className="connect__button">Connect Wallet</button>
+            <button onClick={connect} className="connect__button">Connect Wallet</button>
         </div>
     )
 }
